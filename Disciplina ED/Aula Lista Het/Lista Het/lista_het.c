b4ca5898b88adcd14972184bd5888fad8aa9c64e
dautin-ui <adauto123rn@gmail.com>
2026-02-06 14:36:33 -0300

    Update lista_het.c

--- Disciplina ED/Aula Lista Het/Lista Het/lista_het.c
+++ Disciplina ED/Aula Lista Het/Lista Het/lista_het.c
@@ -5,23 +5,3 @@
 #define RET 0
 #define TRI 1
 #define CIR 2
-
-struct listaHet {
-    int tipo;
-    void* info;
-    struct listaHet* prox;
-};
-
-struct retangulo {
-    float b;
-    float h;
-};
-
-struct triangulo {
-    float b;
-    float h;
-};
-
-struct circulo {
-    float r;
-};
